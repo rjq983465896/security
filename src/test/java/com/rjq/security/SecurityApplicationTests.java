d196ac9a1920af2d8d3bed73b853a8596f7d2068
renjingqian <983465896@qq,com>
2021-11-28 23:59:11 +0800

    init

---
+++ src/test/java/com/rjq/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjq.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
